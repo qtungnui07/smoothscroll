e954d0b68852c733ad5fb1a3caa6893eada3b744
qtungnui07 <qtung.07.dt@gmail.com>
2024-08-02 05:49:12 +0700

    create

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    reset:true,
+    distance: '60px',
+    duration: 2500,
+    delay:200
+});
+ScrollReveal().reveal('.main-title,.section-title',  { delay: 100 , origin:'left'});
+ScrollReveal().reveal('.sec-01,.image',  { delay: 350 , origin:'bottom'});
+ScrollReveal().reveal('.sec-02,.image',  { delay: 350 , origin:'bottom'});
+ScrollReveal().reveal('.text-box',  { delay: 400 , origin:'right'});
+ScrollReveal().reveal('.media-icons i',  { delay: 350 , origin:'bottom', interval:200});
+ScrollReveal().reveal('.media-info li',  { delay: 350 , origin:'left', interval:200});
